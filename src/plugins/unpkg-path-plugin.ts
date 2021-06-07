e624226747ec29e6d77fca78b9e4330cafe96acf
Aman Srivastava <aman11srivastava@gmail.com>
2021-06-08 02:01:42 +0530

    Bundling from user's input from textbox

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,7 +1,12 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios'
+import localForage from "localforage";
 
-export const unpkgPathPlugin = () => {
+const fileCache = localForage.createInstance({
+    name: 'fileCache'
+});
+
+export const unpkgPathPlugin = (inputCode: string) => {
     return {
         name: 'unpkg-path-plugin',
         setup(build: esbuild.PluginBuild) {
@@ -29,19 +34,23 @@ export const unpkgPathPlugin = () => {
                 if (args.path === 'index.js') {
                     return {
                         loader: 'jsx',
-                        contents: `
-                        import React, {useState} from 'react';
-                        console.log(React, useState);
-                        `,
+                        contents: inputCode,
                     };
                 }
+
+                const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+                if (cachedResult){
+                    return cachedResult
+                }
+
                 const {data, request} = await axios.get(args.path);
-                // console.log(request)
-                return {
+                 const result: esbuild.OnLoadResult = {
                     loader: 'jsx',
                     contents: data,
                     resolveDir: new URL('./', request.responseURL).pathname
                 }
+                await fileCache.setItem(args.path, result)
+                return result
             });
         },
     };
